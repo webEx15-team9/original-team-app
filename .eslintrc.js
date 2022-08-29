504f232da1e02afd2e25919be50978255c093c1b
KoutarouY <kou06yokohama@gmail.com>
2022-08-30 02:10:28 +0900

    eslint修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,6 @@ module.exports = {
   },
 
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-};
+
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+}
