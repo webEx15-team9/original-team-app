fb932faead1a32d990642023e1a03991aa0d0a71
ChihiroMorimoto <k10fwnjsk7867.wfty@gmail.com>
2022-09-07 21:41:58 +0900

    スライドショーできなかった

--- src/views/SlideApp.vue
+++ src/views/SlideApp.vue
@@ -2,46 +2,25 @@
   <div class="sliderImage">
     <carousel :per-page="2" :speed="500">
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E3%82%B9%E3%82%AB%E3%82%A4%E3%83%84%E3%83%AA%E3%83%BC.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/スカイツリー.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E4%BC%8A%E5%8B%A2%E7%A5%9E%E5%AE%AE.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/伊勢神宮.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E5%8E%B3%E5%B3%B6%E7%A5%9E%E7%A4%BE.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/清水寺.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E5%A4%A9%E6%A9%8B%E7%AB%8B.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/石垣島.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E5%AF%8C%E5%A3%AB%E5%B1%B1.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/天橋立.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E6%B8%85%E6%B0%B4%E5%AF%BA.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/富士山.jpg" class="zekkeiPhoto" />
       </slide>
       <slide>
-        <img
-          src="https://github.com/webEx15-team9/original-team-app/blob/slide/src/assets/%E7%9F%B3%E5%9E%A3%E5%B3%B6.jpg?raw=true"
-          class="zekkeiPhoto"
-        />
+        <img src="@/assets/厳島神社.jpg" class="zekkeiPhoto" />
       </slide>
       <template #addons>
         <navigation />
