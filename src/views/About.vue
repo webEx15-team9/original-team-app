c4e71e49b5126937d625597b17e5d7785781859b
mimi0801 <kasumimi0801@gmail.com>
2022-09-07 19:57:54 +0900

    Aboutページ作りました

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,37 @@
 <template>
-  <div class="about">
-    <div>This is an about page</div>
+  <span class="about">About</span>
+  <h2></h2>
+  <img
+    class="about-image"
+    src="https://ptl.imagegateway.net/contents/original/photocontest/47pref2018/result/img/area/s_yamanashi07.jpg"
+    alt="画像1"
+  />
+  <div class="content">
+    <h4>サイト説明</h4>
+    <h6>このサイトは日本各地の素敵な景色をたくさん見れるサイトです！</h6>
+    <h6>素敵な景色を見つけたら投稿ページからシェアしてね！</h6>
+    <h6>みんなの投稿待ってます☆</h6>
+    <img
+      class="about-image"
+      src="https://ptl.imagegateway.net/contents/original/photocontest/47pref2018/result/img/area/s_yamanashi07.jpg"
+      alt="画像1"
+      width="300"
+    />
+  </div>
+
+  <div>
+    <button v-on:click="googleLogin">ログイン</button>
   </div>
 </template>
 
+<script>
+export default {}
+</script>
+
 <style>
 .about {
-  font-size: 30px;
+  font-size: 50px;
+  padding-top: 20px;
+  border-bottom: solid;
 }
 </style>
